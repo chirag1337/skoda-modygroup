e6e7dc51f749f7f9da6cae6c452fa210b0a42b72
AjayKushwaha25 <ajay@ottoedge.com>
2026-04-13 18:57:29 +0530

    toyota visakhapatnam analytics code added

--- toyota/visakhapatnam/index.php
+++ toyota/visakhapatnam/index.php
@@ -7,7 +7,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NNVCCWPT');</script>
+    })(window,document,'script','dataLayer','GTM-K3VGSRRV');</script>
     <!-- End Google Tag Manager -->
 
     <meta charset="utf-8">
@@ -66,7 +66,7 @@
 
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNVCCWPT"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3VGSRRV"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
